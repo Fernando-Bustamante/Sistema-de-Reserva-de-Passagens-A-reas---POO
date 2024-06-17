9ef9eee1bad98561b2aa85acddc3d80565794458
Fernando-Bustamante <147199698+Fernando-Bustamante@users.noreply.github.com>
2024-06-17 10:13:10 -0300

    Update UserManager.py

--- Build/UserManager.py
+++ Build/UserManager.py
@@ -2,17 +2,16 @@ from Build.User import User
 
 class UserManager:
     def __init__(self, System):
-        self.users_ = []  # Lista para armazenar os usuários
-        self._carregar_usuarios()  # Carregar usuários do arquivo no momento da inicialização
+        self.users_ = []
+        self.System = System
+        self._carregar_usuarios()
 
     def _carregar_usuarios(self):
-        # Método privado para carregar usuários do arquivo "passengers.txt"
         try:
-            with open("passengers.txt", "r", encoding='utf-8') as arquivo:
+            with open("Data/passengers.txt", "r", encoding='utf-8') as arquivo:
                 lines = arquivo.readlines()
                 i = 0
                 while i < len(lines):
-                    # Lendo os dados de cada usuário do arquivo
                     name = lines[i].strip()
                     email = lines[i + 1].strip()
                     password = lines[i + 2].strip()
@@ -22,35 +21,35 @@ class UserManager:
                     flight_info = []
 
                     for j in range(num_tickets):
-                        # Lendo informações de voos do usuário
                         flight_line = lines[i + 6 + j].strip().split()
-                        flight_info.append((int(flight_line[0]), int(flight_line[1])))
+                        flight_info.append({
+                            "voo": int(flight_line[0]),
+                            "seat": int(flight_line[1])
+                        })
 
-                    # Dados do usuário formatados em um dicionário
-                    user_data = {
-                        'name': name,
-                        'email': email,
-                        'password': password,
-                        'cpf': cpf,
-                        'credit_card': credit_card,
-                        'flight_info': flight_info
-                    }
-                    self._cadastro_usuario(user_data)  # Cadastro do usuário utilizando o método privado
+                    user = User(name, email, password, cpf, credit_card, flight_info)
+                    self.users_.append(user)
                     i += 6 + num_tickets
         except FileNotFoundError:
             print("Erro ao abrir o arquivo passengers.txt")
 
     def _cadastro_usuario(self, user_data):
-        # Método privado para criar um objeto User e adicioná-lo à lista de usuários
-        user = User(user_data)
+        user = User(**user_data)
         self.users_.append(user)
 
     def addUser(self, passenger):
-        # Método público para adicionar um novo usuário, utilizando o método privado de cadastro
-        self._cadastro_usuario(passenger)
+        user_data = {
+            'name_': passenger[0],
+            'email_': passenger[1],
+            'password_': passenger[2],
+            'cpf_': int(passenger[3]),
+            'credit_card': int(passenger[4]),
+            'tickets_': passenger[5]
+        }
+        self._cadastro_usuario(user_data)
+        self.atualizar_arquivo_usuarios()
 
     def returnUser(self, name, password):
-        # Método para retornar um usuário que corresponda ao nome e senha fornecidos
         for user in self.users_:
             if user.checkName(name):
                 if user.checkPassword(password):
@@ -59,28 +58,31 @@ class UserManager:
         raise ValueError("erro")
 
     def UpdatePassenger(self):
-        # Método para atualizar o arquivo "passengers.txt" com os dados atuais dos usuários
-        with open("passengers.txt", "w", encoding='utf-8') as arquivo:
-            for user in self.users_:
-                profile_data = user.ReturnProfile()
-                arquivo.write(profile_data["name"] + "\n")
-                arquivo.write(profile_data["email"] + "\n")
-                arquivo.write(profile_data["password"] + "\n")
-                arquivo.write(str(profile_data["cpf"]) + "\n")
-                arquivo.write(str(profile_data["credit_card"]) + "\n")
-                arquivo.write(str(len(profile_data["flight_info"])) + "\n")
-                for flight_info in profile_data["flight_info"]:
-                    arquivo.write(str(flight_info[0]) + " " + str(flight_info[1]) + "\n")
+        self.atualizar_arquivo_usuarios()
+
+    def atualizar_arquivo_usuarios(self):
+        try:
+            with open("Data/passengers.txt", "w", encoding='utf-8') as arquivo:
+                for user in self.users_:
+                    profile_data = user.ReturnProfile()
+                    arquivo.write(profile_data["name"] + "\n")
+                    arquivo.write(profile_data["email"] + "\n")
+                    arquivo.write(profile_data["password"] + "\n")
+                    arquivo.write(str(profile_data["cpf"]) + "\n")
+                    arquivo.write(str(profile_data["credit_card"]) + "\n")
+                    arquivo.write(str(len(profile_data["flight_info"])) + "\n")
+                    for flight_info in profile_data["flight_info"]:
+                        arquivo.write(str(flight_info[0]) + " " + str(flight_info[1]) + "\n")
+        except Exception as e:
+            print(f"Erro ao atualizar o arquivo: {e}")
 
     def validName(self, name):
-        # Método para verificar se um nome de usuário é válido (não duplicado)
         for user in self.users_:
             if user.checkName(name):
                 return False
         return True
 
     def validPassword(self, name, password):
-        # Método para verificar se a senha é válida para um determinado nome de usuário
         for user in self.users_:
             if user.checkName(name):
                 if user.checkPassword(password):
